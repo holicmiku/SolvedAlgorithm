fb790dece0f31b792b39faec8343e2a724db113c
holicmiku <holicmiku.15@gmail.com>
2023-07-19 22:08:52 +0900

    Solved.ac CLASS 2

---
+++ BOJ/CLASS 2/부녀회장이_될테야.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int getNum(int k, int n){
+    if(k==0){
+        return n;
+    }
+
+    if(n==1){
+        return 1;
+    }
+
+    return (getNum(k-1,n) + getNum(k, n-1));
+}
+
+
+int main(){
+    int T,k,n;
+
+    cin >> T;
+
+    for(int i=0; i<T; i++){
+        cin >> k;
+        cin >> n;
+    cout << getNum(k,n) <<"\n";
+
+
+    }
+
+
+
+
+
+}
